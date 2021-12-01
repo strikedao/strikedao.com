f9cc76f4dcc0a7daee4b90ea16bc67d81971b1de
Tim Daubenschütz <tim@daubenschuetz.de>
2021-12-01 16:46:03 +0100

    Fix handler tests that broken w/ switch to POST
    - Fixes #45

--- test/handlers_test.mjs
+++ test/handlers_test.mjs
@@ -9,43 +9,49 @@ import config from "../config.mjs";
 test.afterEach.always(delDB);
 test.before(delDB);
 
-test("if handling allocations works", async t => {
+test.serial("if handling allocations works", async t => {
   await initDB();
 
   const response = await fastify.inject({
-    method: "PATCH",
+    method: "POST",
     url: "/stills/",
     body: {
       email: "example@example.com"
     }
   });
 
-  t.is(response.statusCode, 200);
+  t.is(response.statusCode, 302);
+  t.is(response.headers.location, "/");
 });
 
-test("if handling multiple query strings as an array works with fastify", async t => {
-  await initDB();
+test.serial(
+  "if handling multiple query strings as an array works with fastify",
+  async t => {
+    await initDB();
 
-  const response = await fastify.inject({
-    method: "get",
-    url: "/ballotbox/",
-    query: {
-      tokens: ["a", "b"]
-    }
-  });
+    const response = await fastify.inject({
+      method: "get",
+      url: "/ballotbox/",
+      query: {
+        tokens: ["a", "b"]
+      }
+    });
 
-  t.is(response.statusCode, 200);
-});
+    t.is(response.statusCode, 200);
+  }
+);
 
-test("if handling allocation returns error when not enough stills are available", async t => {
-  migrations.init(0);
-  migrations.init(1);
-  await stills.init();
+test.serial(
+  "if handling allocation returns error when not enough stills are available",
+  async t => {
+    migrations.init(0);
+    migrations.init(1);
+    await stills.init();
 
-  const db = init();
-  const leftovers = config.stills.perEmail - 2;
-  const allocation = config.stills.quantity - leftovers;
-  const statement = db.prepare(`
+    const db = init();
+    const leftovers = config.stills.perEmail - 2;
+    const allocation = config.stills.quantity - leftovers;
+    const statement = db.prepare(`
       UPDATE
         stills
       SET
@@ -53,17 +59,18 @@ test("if handling allocation returns error when not enough stills are available"
       WHERE
         priority = @id
       `);
-  for (let id of Array(allocation).keys()) {
-    statement.run({ id });
-  }
-
-  const response = await fastify.inject({
-    method: "PATCH",
-    url: "/stills/",
-    body: {
-      email: "example@example.com"
+    for (let id of Array(allocation).keys()) {
+      statement.run({ id });
     }
-  });
 
-  t.is(response.statusCode, 410);
-});
+    const response = await fastify.inject({
+      method: "POST",
+      url: "/stills/",
+      body: {
+        email: "example@example.com"
+      }
+    });
+
+    t.is(response.statusCode, 410);
+  }
+);
